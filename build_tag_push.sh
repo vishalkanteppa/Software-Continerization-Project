3a6a8704f172a888f9065072e0744e2f1057c6f6
vishalkanteppa <vishalkanteppa@gmail.com>
2023-02-02 15:30:03 +0100

    Added ajax call for form

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -2,3 +2,7 @@
 docker build -t rest-api:latest rest-api
 docker tag rest-api:latest localhost:5000/scproj/rest-api:latest
 docker image push localhost:5000/scproj/rest-api:latest
+
+docker build -t frontend:latest frontend
+docker tag frontend:latest localhost:5001/scproj/frontend:latest
+docker image push localhost:5001/scproj/frontend:latest
